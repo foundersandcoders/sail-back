8e4b0281cd3abc2580ffcfdd79874a8c093164ec
besartshyti <besartshyti@gmail.com>
2015-06-10 19:11:32 +0100

    update database connections

--- config/env/staging.js
+++ config/env/staging.js
@@ -1,35 +1,26 @@
 module.exports = {
-
 	connections : {
-		localMySql: {
-			adapter: 'sails-mysql',
-			host: 'localhost',
-			user: 'root',
-			password: 'correct',
-			database: 'foch_testing'
+		postgresql: {
+			adapter: 'sails-postgresql',
+			pool: false,
+			ssl: true,
+			url: process.env.PG_URL
 		}
 	},
 	models: {
-		connection: 'localMySql',
-		migrate: 'alter'
+		connection: 'postgresql',
+		migrate: 'safe'
 	},
-
 	session: {
 		secret: '496a95f915e063812b4c39d698db2462',
 		cookie: {
 			maxAge: 14 * 24 * 60 * 60 * 1000,
-			// domain: process.env.DOMAIN || 'localhost'
 		},
 		// In production, uncomment the following lines to set up a shared redis session store
 		// that can be shared across multiple Sails.js servers
 		// adapter: process.env.NODE_ENV == "testing" ? "memory" : 'redis',
 		// adapter: process.env.NODE_ENV == "development" ? "memory" : 'redis',
-		// adapter: 'memory'
-		adapter: 'redis',
-		host: 'localhost',
-		port: 6379,
-		db: 'redistogo',
-		// pass: '13ec14563ee8c74b410535d17b7b2b4f'
+		// adapter: 'redis'
 	},
-	port: process.env.PORT || 1337
+	port: 443
 };
