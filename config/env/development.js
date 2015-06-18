d8f669fd537e47fc7eaf13f89adda520491fdcec
besartshyti <besartshyti@gmail.com>
2015-06-18 12:26:03 +0100

    put back dev config db

--- config/env/development.js
+++ config/env/development.js
@@ -1,26 +1,38 @@
 module.exports = {
-	connections : {
-		postgresql: {
-			adapter: 'sails-postgresql',
-			pool: false,
-			ssl: true,
-			url: "postgres://yaotanyvicavvo:43Qp3gsYz6ooyUB58CQjzT8QZ7@ec2-54-247-79-142.eu-west-1.compute.amazonaws.com:5432/dch5gjaatr84hq"
+	// connections : {
+	// 	postgresql: {
+	// 		adapter: 'sails-postgresql',
+	// 		pool: false,
+	// 		ssl: true,
+	// 		url: "postgres://yaotanyvicavvo:43Qp3gsYz6ooyUB58CQjzT8QZ7@ec2-54-247-79-142.eu-west-1.compute.amazonaws.com:5432/dch5gjaatr84hq"
+	// 	}
+	// },
+	// models: {
+	// 	connection: 'postgresql',
+	// 	migrate: 'safe'
+	// },
+	connections: {
+		localMySql: {
+			adapter: 'sails-mysql',
+			host: 'localhost',
+			user: 'root',
+			password: 'correct',
+			database: 'foch_testing'
 		}
 	},
 	models: {
-		connection: 'postgresql',
-		migrate: 'safe'
+		connection: 'localMySql',
+		migrate: 'save'
 	},
 	session: {
 		secret: '496a95f915e063812b4c39d698db2462',
 		cookie: {
 			maxAge: 14 * 24 * 60 * 60 * 1000,
 		},
-		// In production, uncomment the following lines to set up a shared redis session store
-		// that can be shared across multiple Sails.js servers
-		// adapter: process.env.NODE_ENV == "testing" ? "memory" : 'redis',
-		// adapter: process.env.NODE_ENV == "development" ? "memory" : 'redis',
-		// adapter: 'redis'
+		adapter: 'redis',
+		host: 'localhost',
+		port: 6379,
+		db: 'redistogo'
 	},
 	port: process.env.PORT || 1337
 };
