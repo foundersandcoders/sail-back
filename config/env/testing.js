7d4207a46bc10c6d65667b6deace46609a0c7ce3
besartshyti <besartshyti@gmail.com>
2015-06-20 12:40:56 +0100

    set up sessions in testing mode

--- config/env/testing.js
+++ config/env/testing.js
@@ -4,5 +4,11 @@ module.exports = {
 			adapter: 'sails-memory'
 		},
 	},
+	session: {
+		secret: '496a95f915e063812b4c39d698db2462',
+		cookie: {
+			maxAge: 14 * 24 * 60 * 60 * 1000,
+		}
+	},
 	port: 2000
 };
