235899b89b85b1dfd3e8dac9b4b64407dc6b5983
rjmk <dear.rafe@gmail.com>
2015-09-09 16:07:04 +0100

    adds script for IRC pomo timing

---
+++ pomodoro.sh
@@ -0,0 +1,1 @@
+echo 'POMODORO STARTED' && sleep 600 && echo '10 minutes left this pomodoro' && sleep 300 && echo '5 minutes left this pomodoro' && sleep 300 && echo 'POMODORO OVER, BITCHES! @rjmk @filwisher'
