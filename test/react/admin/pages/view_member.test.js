c0091ae6e1bb0c6f11c8536ab658d69fab0f2528
rjmk <dear.rafe@gmail.com>
2015-09-20 16:48:29 +0100

    s

--- test/react/admin/pages/view_member.test.js
+++ test/react/admin/pages/view_member.test.js
@@ -4,11 +4,13 @@ var test = require('tape')
 var React = require('react/addons')
 var Component = require('../../../../src/admin/pages/view_member.js')
 var arrayify = require('../../../../src/utils/arrayify.js')
-var change = React.addons.TestUtils.Simulate.change
 var click = React.addons.TestUtils.Simulate.click
+var change = React.addons.TestUtils.Simulate.change
 
 var member = require('../../../../src/mock_member.js')
 var events = require('../../../../src/mock_events.js')
+
+var node = document.body
 
 Component.__set__('request', function (opts, cb) {
   opts.uri.match(/\/events/) ?
@@ -64,9 +66,7 @@ test('should render payments section', function (t) {
 
     t.ok(node.innerHTML.indexOf('Payment info') > -1)
     t.ok(node.innerHTML.indexOf('subscription_btn') > -1)
-    t.end()
-  })
-})
+    t.end()})})
 
 test('should render events section', function (t) {
 
@@ -87,77 +87,78 @@ test('should render events section', function (t) {
 })
 
 test('should toggle between edit and view mode', function (t) {
-
   var node = document.body
-  function get_data_nodes () {
-    return arrayify(node.querySelectorAll('.member-info-content .info')).map(function (node) {
-      return node.nextSibling })}
-
-  function check_nodes_tag (tag, nodes) {
-    return nodes.every(function (node){
-      return node.tagName.toLowerCase() === tag })}
-
   function assert_all_nodes_of_tag (tag, nodes) {
     t.ok(check_nodes_tag(tag, nodes)) }
 
   assert_all_nodes_of_tag('span', get_data_nodes())
   node.querySelector('#edit-member-mode').click()
   process.nextTick(function () {
-
     assert_all_nodes_of_tag('input', get_data_nodes())
+    node.querySelector('#cancel').click()
     t.end()
   })
 })
+
+test('field values should update', function (t) {
+  click('#edit-member-mode')
+  var nodes = get_data_nodes()
+  var input = nodes[Math.floor(Math.random() * nodes.length)]
+  input.value = 'random val'
+  change(input.value)
+  process.nextTick(function () {
+    t.equal(input.value, 'random val')
+    t.end()})})
 
 test('should be able to add charges', function (t) {
 
 
   var node = document.body
-  
+
   React.render(React.createElement(Component, {
     member: member,
     params: {
       id: 1234
     }
   }), node, function () {
- 
+
     var fields =['event', 'subscription', 'donation', 'payment']
-    
-    t.ok(fields.every(function(field){ 
+
+    t.ok(fields.every(function(field){
       return !!document.querySelector('#'+field+'_btn')
     }), 'all field buttons present')
     fields.forEach(function (field, i) {
-      
+
       t.test('checking rendering of ' + field, function (st) {
-      
+
         var button = document.querySelector('#'+field+'_btn')
         click(button)
         process.nextTick(function () {
-          
+
           st.ok(document.querySelector('.' + field), field + ' displaying')
           st.end()
         })
       })
     })
-    
-    
-   /* 
+
+
+   /*
     t.test(function (st) {
-    
+
       var button = document.querySelector('#event_btn')
       click(button)
       process.nextTick(function () {
-         
+
         st.ok(document.querySelector('.event'), 'event displaying')
         st.end()
       })
     })
-  
+
  */
     /*
     click(document.querySelector('#event_btn'))
     process.nextTick(function () {
-       
+
       var check = document.querySelector('.event')
       t.ok(check, 'ok@')
       t.end()
@@ -165,7 +166,7 @@ test('should be able to add charges', function (t) {
     */
 /*
     fields.forEach(function (field) {
-    
+
       click(document.querySelector('#'+field+'_btn'))
       var check = document.querySelector('#hohoho')
       t.notOk(check, 'not ok@')
@@ -178,11 +179,20 @@ test('should be able to add charges', function (t) {
         // t.ok(document.querySelector('h2.'+field), field +' header exists')
       })
     })
-  */  
+  */
     /* TODO: test clicks on buttons  */
     /* TODO: test input of fields  */
     /* TODO: test save on fields  */
-   // node.querySelector('h1')  
+   // node.querySelector('h1')
    // t.end()
   })
 })
+
+function get_data_nodes () {
+  return arrayify(node.querySelectorAll('.member-info-content .info')).map(function (node) {
+    return node.nextSibling })}
+
+function check_nodes_tag (tag, nodes) {
+  return nodes.every(function (node){
+    return node.tagName.toLowerCase() === tag })}
+
