14e560ca796558dfbb57dfb71c504bd04dcbe538
rjmk <dear.rafe@gmail.com>
2015-09-23 12:57:06 +0100

    adds date picker and fixes month ordering bug

--- src/admin/components/common/make_charge_fields.js
+++ src/admin/components/common/make_charge_fields.js
@@ -22,60 +22,58 @@ module.exports = function make_charge_form (charge, fields) {
         amount: '',
         reference: '',
         type: '',
-        notes: ''
-      } 
-    },
+        notes: '' }},
+
     back: function () {
-      this.props.click('payments-table')   
-    },
+      this.props.click('payments-table')},
+
     change: function (e) {
       var state = require('../../../utils/clone')(this.state)
       state[e.target.id] = e.target.value
-      this.setState(state) 
-    },
+      this.setState(state)},
+
     save: function () {
-    
+
       var self = this
       var record = require('../../../utils/clone')(self.state)
-      
+
+
       record.description = to_title_case(charge)
       record.category = charge
       record.member = self.props.mid
-      
+
       request({
         method: 'POST',
         uri: '/api/payments',
         json: record
       }, function (err, res, body) {
-        
+
         self.setState({
           date: '',
           amount: '',
           reference: '',
           type: '',
-          notes: ''
-        })
-      })
-    },
+          notes: '' })})},
+
     render: function () {
       var rendered_fields = fields.map(function (field, i) {
         return (
           <div>
-            <input type='text' onChange={this.change} id={field} 
-            value={this.state[field]} placeholder={field} key={i} />
+            <input type={field === 'date' ? 'date' :'text'} onChange={this.change}
+                id={field} value={this.state[field]} placeholder={field} key={i} />
             <div className='inner-section-divider-small'></div>
-          </div> 
-        )
-      }.bind(this))
+          </div>
+        )}.bind(this))
+
       return (
         <div className='container-small'>
           <div className='inner-section-divider-medium'></div>
           <h2 className={charge}>{to_title_case(charge)}</h2>
           {rendered_fields}
-          <button onClick={this.back} className='align-one btn-primary'>Back</button>
-          <button onClick={this.save} className='align-two btn-primary'>Save</button>
-        </div>
-      ) 
-    }
-  })
-}
+          <button onClick={this.back} className='align-one btn-primary'>
+            Back
+          </button>
+          <button onClick={this.save} className='align-two btn-primary'>
+            Save
+          </button>
+        </div> )}})}
