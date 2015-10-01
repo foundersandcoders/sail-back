782be435c9622f8ce55b5d4cd36c05719eebc822
rjmk <dear.rafe@gmail.com>
2015-10-01 11:11:08 +0100

    highlight event row on hover

--- src/admin/components/events_table.js
+++ src/admin/components/events_table.js
@@ -4,6 +4,8 @@ var Table = require('./table')
 module.exports = React.createClass({
   render: function () {
 
+            console.log('SEKJFDK',this.props.selected)
+
     var headers = ['Date', 'Reference', 'Short Description',
       'Time', 'Location', 'Host', 'Price per member',
       'Price per guest', 'Max number of guests', 'Total places available',
@@ -15,5 +17,9 @@ module.exports = React.createClass({
       .map(function (event) {
         return headers.map(get_entry_for_event(event))})
 
-    return (<Table className='events-table' data={[headers, entries]} />)}})
+    return (<Table
+        {...this.props}
+        className='events-table'
+        data={[headers, entries]}
+      />)}})
 
