261dbc0e6e76a5313d0ccb85fd060e1646250719
rjmk <dear.rafe@gmail.com>
2015-11-09 17:42:32 +0000

    massages data nicely

--- src/admin/pages/reports.js
+++ src/admin/pages/reports.js
@@ -1,62 +1,57 @@
 'use strict'
 
 var React = require('react')
-var Table = require('../components/table')
-var get_entry = require('app/get_entry')(' ')
+
 var object_assign = require('object-assign')
-var compose = require('fn-compose')
+var deep_equal = require('deep-equal')
 var curry = require('curry')
+var compose = require('fn-compose')
 
-var make_payments_with_balance = require('app/make_payments_with_balance')
+module.exports = React.createClass({
+  propTypes: {
+    payments: React.PropTypes.array,
+    charges: React.PropTypes.object },
 
-var Reports = React.createClass({
   render: function () {
-    var headers = ['Date Paid', 'Member', 'Surname', 'Type',
-        'Subscription', 'Donation', 'Events',
-        'Balance Due', 'Payment', 'Balance Outstanding']
-    var payments = require('../../../test/mocks/ref-payments.json')
-    var charges = require('../../../test/mocks/charges.json')
-    var entries = payments.map(get_relevant_charges(charges))[1].map(build_row)
-    var data = [headers, entries]
-    return (
-      <Table data={data} className='report-table' />)} })
+    return <div charges={get_charges(this.props.payments, this.props.charges)}></div> } })
 
-var convert = compose(add_value, flatten_props)
+function user_entry_from_payment ({member, date, amount}) {
+  return {
+    member_number: member,
+    payment_date: date,
+    subscription: 0,
+    donation: 0,
+    events: 0,
+    payments: amount,
+    payments_not_on_this_ref: 0,
+    balance_due: -amount
+  } }
 
-var build_row = compose(make_row, convert)
+var get_charges = function get_charges (payments, charges) {
+  return payments.map(compose(add_relevant_charges(charges), user_entry_from_payment)) }
 
-function flatten_props (payment) {
-  return object_assign({}, payment, {
-    member_id: payment.member.id,
-    surname: payment.member.last_name,
-    amount: payment.amount }) }
+var add_relevant_charges = curry(function add_relevant_charges (charges, payment) {
+  return get_relevant_charges(charges, payment).reduce(add_charge, payment) })
 
-function add_value (payment) {
-  var payment_vals = {}
-  payment_vals[payment.category] = payment.amount;
-  return object_assign(payment_vals, payment) }
+var get_relevant_charges = curry(function get_relevant (charges, payment) {
+  return charges[payment.member_number].filter(earlier_and_not_same(payment)) })
 
-var get_relevant_charges = curry(function (charges, payment) {
-  return since_last_zero(charges.filter(dated_before(payment.date))) })
+var add_charge = function add_charge (entry, { category, amount } ) {
+  return object_assign({}, entry, {
+    [field(category)]: amount + entry[field(category)],
+    balance_due: correct_sign(category, amount) + entry.balance_due }) }
 
-function make_row (payment) {
-  return ['date', 'member_id', 'surname', 'type', 'subscription', 'donation',
-      'events', 'balance due', 'payment'].map(get_entry(payment)) }
+var earlier_and_not_same = curry.to(2, function (a, b) {
+  return not_same(a,b) && earlier(a, b) })
 
-function since_last_zero (payments) {
-  var payments_with_balance = make_payments_with_balance(payments)
-  var last_zero = last(payments_with_balance.filter(balance_is_zero))
-  var last_index = payments_with_balance.indexOf(last_zero)
-  var slice_from = last_index < 0 ? 1 : last_index
-  return payments_with_balance.slice(slice_from) }
+var not_same = function not_same (a, b) {
+  return !deep_equal(a, user_entry_from_payment(b)) }
 
-var dated_before = curry(function (date, dated) {
-  return dated.date && dated.date < date })
+var earlier = function earlier (entry_a, entry_b) {
+  return new Date(entry_a.payment_date) >= new Date(entry_b.date) }
 
-function last (array) {
-  return array.reverse()[0] }
+var correct_sign = function correct_sign (category, amount) {
+  return (category === 'payment' ? -1 : 1) * amount }
 
-function balance_is_zero (payment) {
-  return payment['balance due'] === 0 }
-
-module.exports = Reports
+var field = function (category) {
+  return category === 'payment' ? 'payments_not_on_this_ref' : category }
--- test/react/tests.js
+++ test/react/tests.js
@@ -9,6 +9,7 @@ require('./admin/pages/data_maintenance.test.js')
 require('./admin/pages/home.test.js')
 require('./admin/pages/view_member.test.js')
 require('./admin/pages/add_event.test.js')
+require('./admin/pages/reports.test.js')
 
 // open app tests
 require('./open/signin.test.js')
