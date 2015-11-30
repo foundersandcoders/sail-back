aa0f9e24f2839904235a7d9a6fd5e24a61f5a52b
rjmk <dear.rafe@gmail.com>
2015-11-30 15:36:31 +0000

    bug fix

--- src/admin/pages/payments.js
+++ src/admin/pages/payments.js
@@ -32,7 +32,7 @@ module.exports = React.createClass({
             submit_handler={search(get_payments, this.setState.bind(this))}
             input_or_select={this.props.input_or_select}
             inputs={this.props.inputs} />
-        {Object.keys(this.state.charges).length ?
+        {number_of_keys(this.state.charges) > 0 ?
             <PayingIn
                 payments={this.state.payments}
                 charges={this.state.charges}
@@ -48,45 +48,47 @@ var search = curry(function search (get_payments, set_state, e) {
   e.preventDefault()
   var payments = get_payments(e)
 
-  payments.fork(trace('payments error'), match_ref => {
-    var charges = make_charges(match_ref)
+  payments.fork(trace('payments error'), receive_payments(set_state)) })
 
-    set_state({
-      payments: match_ref
-    })
+var receive_payments = curry((set_state, match_ref) => {
+  var charges = make_charges(match_ref)
+  var charge_data = {}
+  var update_data = more_data => object_assign(charge_data, more_data)
 
-    // Ap doesn't work nicely for data.Task
-    // going to mutate! :cry:
+  set_state({ payments: match_ref })
 
-    var charge_data = {}
+  // Ap doesn't work nicely for data.Task
+  // going to mutate! :cry:
+  charges.forEach(member_charges =>
+    member_charges.fork(trace('charge error'), dethunk(
+        () => set_charges(set_state)
+        , () => update_data ) )) })
 
-    var update_data = more_data => object_assign(charge_data, more_data)
-
-    charges.forEach(member_charges =>
-      member_charges.fork(trace('charge error'), dethunk(
-          () => set_charges(set_state)
-          , () => combine(charge_data)) )) }) })
-
-function get_charges (id) {
-  return get_data('api/payments/?member=' + id).map(function (d) {
-    return { [id]: d } }) }
+var get_member_charges = id =>
+  get_data('api/payments/?member=' + id).map(member_charges =>
+      ({ [id]: member_charges }))
 
 var make_charges = dethunk(
-    () => map(get_charges)
-    , () => filter(first_occurrence)
-    , () => map(prop_or('', 'member')) )
+    () => map(get_member_charges)
+    , () => get_unique_members )
+
+var number_of_keys = object =>
+  Object.keys(object).length
+
+var number_of_members = dethunk(
+  () => arr => arr.length
+  , () => get_unique_members )
+
+var get_unique_members = dethunk(
+  () => filter(first_occurrence)
+  , () => map(prop_or({}, 'member')) )
 
 var set_charges = curry((set_state, charge_data) =>
   set_state({ charges: charge_data }))
 
 var combine = curry((a, b) =>
-    object_assign({}, a, b))
+  object_assign({}, a, b))
 
-function necessary_members (tasks, member) {
-  tasks[member] = get_charges(member)
-  return tasks }
+var first_occurrence = (e, i, a) =>
+  a.indexOf(e) === i
 
-function first_occurrence (e, i, a) {
-  return a.indexOf(e) === i }
-
-function id (x) { return x }
