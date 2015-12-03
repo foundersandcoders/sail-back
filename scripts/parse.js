ff27f130900e286c97cef0bd7e9afdb17737f1c8
rjmk <dear.rafe@gmail.com>
2015-12-03 16:32:57 +0000

    less finnicky script functionality

--- scripts/parse.js
+++ scripts/parse.js
@@ -4,11 +4,13 @@ var fs = require('fs')
 var parse_csv = require('../src/services/parse_csv.js').parse
 
 var type = process.argv[2]
-var csv = fs.readFileSync('../foch-data/' + type + '.csv')
+var csv = fs.readFileSync(__dirname + '../../foch-data/' + type + '.csv')
 
 var result = { type: type, result: csv.toString() }
 
 parse_csv(result, write)
 
 function write (err, json) {
-  fs.writeFileSync('../foch-data/' + type + '.json', JSON.stringify(json)) }
+  fs.writeFileSync(
+      __dirname + '../../foch-data/' + type + '.json'
+      , JSON.stringify(json)) }
