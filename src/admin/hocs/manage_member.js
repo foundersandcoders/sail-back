d0a4114dc4c8096bce9d4db86904b140b89cc768
rjmk <dear.rafe@gmail.com>
2015-12-15 09:24:34 +0000

    makes i.e. work
    Short method syntax was not transpiled.
    This commit removes it.

--- src/admin/hocs/manage_member.js
+++ src/admin/hocs/manage_member.js
@@ -10,29 +10,29 @@ module.exports = curry((Component, start_member, get_member) => {
   var WithMember = React.createClass({
     displayName: Component.displayName || Component.name || 'Component',
 
-    getInitialState () {
+    getInitialState: function () {
       return { member: start_member, errors: [] }
     },
 
-    componentDidMount () {
+    componentDidMount: function () {
       get_member(this.update_member, this.props)
     },
 
-    update_member (new_member) {
+    update_member: function (new_member) {
       this.setState({ member: object_assign({}, this.state.member, new_member) })
     },
 
-    validation_error (errors) {
+    validation_error: function (errors) {
       this.setState({errors: this.state.errors.concat(errors)})
     },
 
-    change_handler (e) {
+    change_handler: function (e) {
       this.update_member(object_assign(
           {},
           this.state.member,
           { [e.target.id]: e.target.value })) },
 
-    verify_member (e, cb = noop) {
+    verify_member: function (e, cb = noop) {
       member_schema.validate(
           this.state.member
           , { abortEarly: false }
@@ -45,7 +45,7 @@ module.exports = curry((Component, start_member, get_member) => {
             })
             cb() }) },
 
-    render () {
+    render: function () {
       return <Component
           update_member={this.update_member}
           verify_member={this.verify_member}
