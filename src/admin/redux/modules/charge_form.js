afa27b4ec357d7b807cd152ca42fb6ecc0d4ca26
rjmk <dear.rafe@gmail.com>
2016-01-27 15:10:13 +0000

    reimplement sorted dates

--- src/admin/redux/modules/charge_form.js
+++ src/admin/redux/modules/charge_form.js
@@ -1,6 +1,7 @@
 const { createAction } = require('redux-actions')
 
 const { ADDED_PAYMENT } = require('./payments.js')
+const { FETCHED_MEMBER } = require('./member.js')
 const SWITCHED_CHARGE_TYPE = 'SWITCHED_CHARGE_TYPE'
 
 const reducer = (charge_type = '', { type, payload }) => {
@@ -8,6 +9,10 @@ const reducer = (charge_type = '', { type, payload }) => {
     case SWITCHED_CHARGE_TYPE:
       return payload
     case ADDED_PAYMENT:
+      return ''
+    case FETCHED_MEMBER:
+      // TODO: Avoid flash of form by connecting router to redux or
+      // signalling fetch event before received
       return ''
     default:
       return charge_type
--- src/admin/redux/modules/payments.js
+++ src/admin/redux/modules/payments.js
@@ -1,6 +1,7 @@
 const { createAction } = require('redux-actions')
 const { post, del } = require('app/http')
-const { compose, flip, replace, concat, map, propOr, filter } = require('ramda')
+const { compose, flip, replace, concat, map, propOr, filter, sortBy } =
+  require('ramda')
 const { standardise } = require('app/transform_dated')
 
 const { FETCHED_MEMBER } = require('./member.js')
@@ -10,9 +11,9 @@ const REMOVED_PAYMENT = 'REMOVED_PAYMENT'
 const reducer = (payments = [], { type, payload }) => {
   switch (type) {
     case FETCHED_MEMBER:
-      return payload.other.payments
+      return sort_dated(payload.other.payments)
     case ADDED_PAYMENT:
-      return concat(payments, [payload])
+      return sort_dated(concat(payments, [payload]))
     case REMOVED_PAYMENT:
       return filter(({id}) => id !== payload, payments)
     default:
@@ -23,6 +24,12 @@ const reducer = (payments = [], { type, payload }) => {
 const DELETION_URL = 'api/payments/'
 
 const make_deletion_url = concat(DELETION_URL)
+
+const sort_dated = sortBy(compose
+  ( (date) => new Date(date).getTime()
+  , propOr(new Date(), 'date')
+  )
+)
 
 const add_payment = createAction
   ( ADDED_PAYMENT
