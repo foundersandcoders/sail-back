e9de7ec2585910543189a13123c546d7af5dba5f
rjmk <dear.rafe@gmail.com>
2016-01-28 10:48:59 +0000

    removing unused files

--- src/shared/on_change.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-module.exports = function on_change (section, e) {
-  var state = require('clone')(this.state)
-  if (!state[section]) state[section] = {}
-  state[section][e.target.id] = e.target.value
-  this.setState(state)
-}
