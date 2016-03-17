5319497044c05b720fd33cf92e8a603d77578710
rjmk <dear.rafe@gmail.com>
2016-03-17 21:46:54 +0000

    no need for if statement anymore

--- src/admin/redux/middleware/heartbeat.js
+++ src/admin/redux/middleware/heartbeat.js
@@ -1,9 +1,13 @@
 const xhr = require('xhr')
+const throttle = require('lodash.throttle')
 
 export default store => next => action => {
-  if (action.type === 'TIMEOUT') return next(action)
-  xhr.get('/admin', (err, res, body) => {
-    if (res.statusCode >= 400) window.location.href = window.location.origin
-  })
+  relogin_timedout()
   return next(action)
 }
+
+const check_admin = cb => throttle(() => xhr.get('/admin', cb), 1000)
+
+const relogin_timedout = check_admin((err, res, body) => {
+    if (res.statusCode >= 400) window.location.href = window.location.origin
+})
