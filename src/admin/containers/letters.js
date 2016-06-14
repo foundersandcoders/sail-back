e23e8276899c855648a778c10b2cc49d0d0e3eb3
spudtrader <jmurphy.web@gmail.com>
2016-06-14 14:42:50 +0100

    load in letters component

---
+++ src/admin/containers/letters.js
@@ -0,0 +1,17 @@
+//TODO learn ramda
+
+import React from 'react'
+import { connect } from 'react-redux'
+
+
+class Letters extends React.Component {
+  render () {
+    return (
+      <div>in letters containeer</div>
+    )
+  }
+}
+
+
+
+export default connect()(Letters)
