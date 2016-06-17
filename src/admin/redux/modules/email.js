7a1f049105feda4c3a0927344ac73100d8b93953
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2016-06-17 11:37:09 +0100

    fix linting errors

--- src/admin/redux/modules/email.js
+++ src/admin/redux/modules/email.js
@@ -1,12 +1,11 @@
 /* @flow */
-const { createAction, handleAction } = require('redux-actions')
+const { createAction } = require('redux-actions')
 const { get_body, post } = require('app/http')
-const { lensPath, over, not, indexBy, map, propOr, merge, ifElse, flip, gte,
-  cond, assoc, where, objOf, zip, set, lift } =
+const { lensPath, over, not, indexBy, map, propOr, merge, ifElse, gte,
+  cond, where, objOf, zip, set, lift } =
       require('ramda')
 const { K, compose, pipe } = require('sanctuary')
 
-const { PATH_UPDATE } = require('./route.js')
 
 const SEND_WELCOME =
   'SEND_WELCOME'
@@ -21,43 +20,43 @@ const TOGGLE_LIST =
 const TOGGLE_CONTENT =
   'TOGGLE_CONTENT'
 
-import type { Action, Reducer } from 'redux'  //eslint-disable-line
+import type { Action, Reducer } from 'redux'  // eslint-disable-line
 
-type State = { emails: { [key: string]: { overdue: number } } }
+type State = { emails: { [key: string]: { overdue: number } } }  //eslint-disable-line
 
 const initialState = { emails: { } }
 
 const reducer : Reducer<State, Action>
   = (state = initialState, { type, payload }) => {
     const update = lens => value => (set(lens, value, state) : State)
-    const emails = lensPath(['emails'])
-    const sent = lensPath(['email_sent'])
-    const list_hidden = lensPath(['list_hidden'])
+    const emails = lensPath([ 'emails' ])
+    const sent = lensPath([ 'email_sent' ])
+    const list_hidden = lensPath([ 'list_hidden' ])
     const new_emails = template => shape =>
       update(emails)(map(template, shape(payload.results)))
     switch (type) {
-      case SEND_SUB_REMINDER:
-        return new_emails(template_subs)(primaries)
-      case SEND_NEWSLETTER:
-        return new_emails(newsletter_alert)(shape_newsletters)
-      case SEND_NEWS_REMINDER:
-        return new_emails(newsletter_reminder)(shape_newsletters)
-      case TOGGLE_LIST:
-        return (over(list_hidden, not, state): State)
-      case TOGGLE_CONTENT:
-        return toggle_show(payload)(state)
-      case SEND_WELCOME:
-        return update(sent)(true)
-      default:
-        return state
+    case SEND_SUB_REMINDER:
+      return new_emails(template_subs)(primaries)
+    case SEND_NEWSLETTER:
+      return new_emails(newsletter_alert)(shape_newsletters)
+    case SEND_NEWS_REMINDER:
+      return new_emails(newsletter_reminder)(shape_newsletters)
+    case TOGGLE_LIST:
+      return (over(list_hidden, not, state): State)
+    case TOGGLE_CONTENT:
+      return toggle_show(payload)(state)
+    case SEND_WELCOME:
+      return update(sent)(true)
+    default:
+      return state
     }
   }
 
 export default reducer
 
-const time_check = pipe([gte, objOf('overdue'), where])
+const time_check = pipe([ gte, objOf('overdue'), where ])
 
-const templating = compose(cond)(zip(map(time_check, [60, 90, Infinity])))
+const templating = compose(cond)(zip(map(time_check, [ 60, 90, Infinity ])))
 
 const placeholder = compose(K)(objOf('content'))
 
@@ -73,8 +72,7 @@ const newsletter_alert = placeholder('1st newsletter email')
 
 const newsletter_reminder = placeholder('2nd newsletter email')
 
-const template_subs = ifElse
-  ( propOr(false, 'standing_order')
+const template_subs = ifElse(propOr(false, 'standing_order')
   , missing_standing_order
   , late_payment
   )
