9250f2b7ef7794253b3d32ccd6b6ab8e9f32bfec
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2016-06-24 18:14:06 +0100

    Get rid of unused letters.js in containers.

--- src/admin/containers/letters.js
+++ src/admin/containers/letters.js
@@ -1,26 +1,36 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import { send_newsletter_post, send_sub_reminder_post } from '../redux/modules/letters/letters.js'
+import { send_newsletter_post
+       , send_sub_reminder_post
+       , active_view
+       } from '../redux/modules/letters/letters.js'
 
 import StandingOrderLetter from '../dumb_components/standing_order_letter.js'
 import LetterRecipients from '../dumb_components/letter_recipients.js'
 
-const Letters = ({ letters, send_newsletter_post, send_sub_reminder_post }) => (
+const Letters = ({ letters, send_newsletter_post, send_sub_reminder_post, active_view }) => {
+  return (
   <div className='top-letter-container'>
     <button onClick={send_newsletter_post}>Show All Post Members</button>
     <button onClick={send_sub_reminder_post}>Print Subscription Reminders</button>
-      <div>
-        {letters.post_members.length > 0
-          ? <LetterRecipients letters={letters.post_members} />
-          : null
-        }
+    <div>
+      <button onClick={() => active_view('post_members', !letters.post_members.shown)}>Show</button>
+      {letters.post_members.members.length > 0 && letters.post_members.shown
+        ? <PostSection letters={letters.post_members.members} />
+        : null
+      }
+      {letters.sub_reminders.length > 0
+        ? generateSubLetters(letters.sub_reminders)
+        : null
+      }
+    </div>
+  </div>
+)}
 
-        {letters.sub_reminders.length > 0
-          ? generateSubLetters(letters.sub_reminders)
-          : null
-        }
-      </div>
+const PostSection = ({ letters }) => (
+  <div>
+    <LetterRecipients letters={letters} />
   </div>
 )
 
@@ -47,4 +57,5 @@ const mapStateToProps = (state) => {
   return { letters: state.letters }
 }
 
-export default connect(mapStateToProps, { send_newsletter_post, send_sub_reminder_post })(Letters)
+export default
+  connect(mapStateToProps, { send_newsletter_post, send_sub_reminder_post, active_view })(Letters)
