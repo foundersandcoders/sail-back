85f1cbd68be4f4682cfd921bdb150b59c9687e71
spudtrader <jmurphy.web@gmail.com>
2016-07-05 18:57:45 +0100

    fix automatic rendering of Hide Members button

--- src/admin/containers/letters.js
+++ src/admin/containers/letters.js
@@ -9,27 +9,28 @@ import { send_newsletter_post
 import PostMembersSection from '../dumb_components/letters/post_members_section.js'
 import SubLettersSection from '../dumb_components/letters/sub_letters_section.js'
 
-const Letters = ({ letters, send_newsletter_post, send_sub_reminder_post, toggle_recipient_list, letters_view }) => {
+const Letters = ({ letters, send_newsletter_post, send_sub_reminder_post, toggle_recipient_list, active_tab }) => {
   return (
     <div className='top-letter-container'>
 
       <button
-        className={'letters-tab' + (letters_view ? '' : ' letters-tab-active')}
+        className={'letters-tab' + (active_tab === 'members' ? ' letters-tab-active' : '')}
         onClick={send_newsletter_post}>
         Post Members
       </button>
 
       <button
-        className={'letters-tab' + (letters_view ? ' letters-tab-active' : '')}
+        className={'letters-tab' + (active_tab === 'letters' ? ' letters-tab-active' : '')}
         onClick={send_sub_reminder_post}>
         Subscription Reminders
       </button>
 
       <div>
-        {letters_view
-          ? <SubLettersSection letters={letters} toggle_recipient_list={toggle_recipient_list} />
-          : <PostMembersSection letters={letters} toggle_recipient_list={toggle_recipient_list}/>
-        }
+        {active_tab &&
+          (active_tab === 'letters'
+            ? <SubLettersSection letters={letters} toggle_recipient_list={toggle_recipient_list} />
+            : <PostMembersSection letters={letters} toggle_recipient_list={toggle_recipient_list}/>)
+          }
       </div>
 
     </div>
@@ -38,7 +39,7 @@ const Letters = ({ letters, send_newsletter_post, send_sub_reminder_post, toggle
 
 const mapStateToProps = (state) => (
   { letters: state.letters
-    , letters_view: state.letters.letters_view
+    , active_tab: state.letters.active_tab
   }
 )
 
