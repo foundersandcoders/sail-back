c7d473682fdde63ac8ac20cac408fd989c0a4cdc
J Murphy <jmurphy.web@gmail.com>
2016-07-21 15:37:50 +0100

    set up redux form component - not working

--- src/admin/dumb_components/sub_due_section.js
+++ src/admin/dumb_components/sub_due_section.js
@@ -1,5 +1,8 @@
 import React from 'react'
 
+import { check_tests, date } from ('app/validate')
+
+
 export default ({ fetch_sub_due, component, checker, ...props }) => {
   const send_request = (e) => {
     e.preventDefault();
@@ -10,12 +13,49 @@ export default ({ fetch_sub_due, component, checker, ...props }) => {
     <div>
       {checker
         ? component(props)
-        : <form className='date-boundaries-form' onSubmit={send_request}>
-            <input type='text' placeholder='From date' />
-            <input type='text' placeholder='To date' />
-            <button type='submit'>{`Submit Subscription's Due`}</button>
-          </form>
+        : <SubDueDates
+            fields={fields}
+            onSubmit={send_request}
+            required={fields}
+          />
       }
     </div>
   )
 }
+
+
+
+const validate = (values) => {
+  const errors = {}
+  if (!values.start) {
+    errors.start = 'start date is required'
+  } else if (values.start < 10) {
+    errors.start = 'start date should be less than ten'
+  }
+  if (!values.end) {
+    errors.end = 'end date is required'
+  } else if (values.end < 10) {
+    errors.end = 'end date should be less than ten'
+  }
+  return errors
+}
+
+
+const SubDueDates = ({fields: {start, end}, handleSubmit, error}) =>
+  <form onSubmit={handleSubmit}>
+    map((field) => <input type='text' placeholder={field} />, fields)
+    <button type='submit'>{`Submit Subscription's Due`}</button>
+  </form>
+
+const fields =
+  [ 'start'
+  , 'end'
+  ]
+
+
+const sub_due_dates = reduxForm(
+  { form: 'sub_due_dates'
+  , validate
+  , fields
+  }
+)(sub_due_dates)
