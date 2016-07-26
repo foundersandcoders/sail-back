fdc7bd016e62b40de7c9a957fbcdd9f413543fdb
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2016-07-26 16:04:38 +0100

    Get rid of post_members_section and fix name on letter address

--- src/admin/redux/modules/letters/letters.js
+++ src/admin/redux/modules/letters/letters.js
@@ -5,8 +5,6 @@ const { merge, compose, objOf, map, props, pick, reduce, liftN, unapply, ifElse,
 
 const { sub_reminder, sub_reminder_SO, subscription_due } = require('./bodies.js')
 
-export const SEND_NEWSLETTER_POST =
-  'SEND_NEWSLETTER_POST'
 export const SEND_SUB_REMINDER_POST =
   'SEND_SUB_REMINDER_POST'
 export const TOGGLE_RECIPIENT_LIST =
@@ -38,8 +36,6 @@ const reducer: Reducer<State, Action>
    const changeTab = compose(assoc('active_tab', type), assoc('shown', false), assoc('shown_letter_index', 0))
 
    switch (type) {
-   case SEND_NEWSLETTER_POST:
-     return changeTab({ ...state, post_members: payload.results })
    case SEND_SUB_REMINDER_POST:
      return changeTab({ ...state, sub_letters: shape(subReminderBody)(payload.results) })
    case SEND_SUBSCRIPTION_DUE_POST:
@@ -60,9 +56,6 @@ export default reducer
 export const show_letter =
   createAction(SHOW_LETTER)
 
-export const send_newsletter_post =
-  createAction(SEND_NEWSLETTER_POST, () => get_body('api/post_members'))
-
 export const send_sub_reminder_post =
   createAction(SEND_SUB_REMINDER_POST, () => get_body('/api/post_sub_reminders'))
 
@@ -81,7 +74,7 @@ const addressProps = [ 'address1', 'address2', 'address3', 'address4', 'county',
 
 const build_address = (member) =>
   concat
-    ( [`${member.title} ${member.first_name || member.initials} ${member.last_name}`]
+    ( [`${member.title} ${member.first_name || member.initials || ''} ${member.last_name}`]
     , props(addressProps)(member)
     )
 
