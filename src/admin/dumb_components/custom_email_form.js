23a94e5a3ff117b056c221fe6ebd60c51cd27dab
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2016-07-26 13:02:28 +0100

    add preview custom email component and style it

--- src/admin/dumb_components/custom_email_form.js
+++ src/admin/dumb_components/custom_email_form.js
@@ -1,18 +1,19 @@
 const React = require('react')
+import preview_custom_email from '../dumb_components/preview_custom_email.js'
 
-export default ({custom_emails: { members }, submit_custom_email }) => {
+export default ({ custom_emails: { members, preview, mode }, preview_custom, ...props}) => {
   const onSubmit = e => {
     e.preventDefault()
-    submit_custom_email(members, e.target)
+    preview_custom([ e.target[0].value, e.target[1].value ])
   }
-  return (
-    <div className='custom-email-container'>
-      <p><i>Write out your custom email.</i></p>
-      <form onSubmit={onSubmit}>
-        <input type='text' className='custom-email-subject' placeholder='Subject'></input>
-        <textarea className='custom-email' placeholder='Email body' />
-        <button type='submit'>Submit</button>
-      </form>
-    </div>
-  )
+  return mode === 'PREVIEW_CUSTOM'
+    ? preview_custom_email({ members, preview, props })
+    : <div className='custom-email-container'>
+        <p><i>Write out your custom email.</i></p>
+        <form onSubmit={onSubmit}>
+          <input type='text' className='custom-email-subject' placeholder='Subject' defaultValue={preview ? preview[0] : ''} />
+          <textarea className='custom-email' placeholder='Email body' defaultValue={preview ? preview[1] : ''}></textarea>
+          <button type='submit'>Preview</button>
+        </form>
+      </div>
 }
