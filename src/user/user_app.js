1be7879c3359c13381acbfbca0e877b44215f271
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2016-07-28 18:31:03 +0100

    added navbar

--- src/user/user_app.js
+++ src/user/user_app.js
@@ -1,14 +1,36 @@
 import React from 'react'
+import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap'
 
 import App from '../shared/app.js'
 
-export default class UserApp extends React.Component{
+export default class UserApp extends React.Component {
   render () {
     return (
-      <App
-        user='User'
-        add_details={this.add_details}
-        children={this.props.children}
-      />
+      <div>
+        {NavbarInstance()}
+        <App
+          user='User'
+          add_details={this.add_details}
+          children={this.props.children}
+        />
+      </div>
   )}
 }
+
+const NavbarInstance = () => (
+  <Navbar>
+    <Navbar.Header>
+      <div className='navigationbar'>
+        <Navbar.Toggle />
+      </div>
+    </Navbar.Header>
+    <Navbar.Collapse>
+      <Nav>
+        <NavItem eventKey={2} href="#">My Details</NavItem>
+        <NavItem eventKey={3} href="#">My Payments</NavItem>
+        <NavItem eventKey={4} href="#">Make a Payment</NavItem>
+        <NavItem eventKey={1} href="/signout">Sign Out</NavItem>
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
+)
