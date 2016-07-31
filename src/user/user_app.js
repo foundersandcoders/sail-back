f866ef23c5c995dbb6dead0638cd091971c8de27
J Murphy <jmurphy.web@gmail.com>
2016-07-31 10:12:51 +0100

    refactor base user app

--- src/user/user_app.js
+++ src/user/user_app.js
@@ -2,13 +2,9 @@ import React from 'react'
 
 import NavBar from './containers/nav_bar.js'
 
-export default class UserApp extends React.Component {
-  render () {
-    const clone_child = child => React.cloneElement(child, { name: this.props.name })
-    return (
-      <div>
-        <NavBar />
-        {React.Children.map(this.props.children, clone_child)}
-      </div>
-  )}
-}
+export default (props) => (
+  <div>
+    <NavBar />
+    {props.children}
+  </div>
+)
