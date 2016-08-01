b96051ca106e287d41a4cb9b337d0789a364dba8
J Murphy <jmurphy.web@gmail.com>
2016-08-01 23:26:44 +0100

    add initial values to my-details and different styling depending on edit mode

--- src/user/components/my_details/details_form.js
+++ src/user/components/my_details/details_form.js
@@ -2,23 +2,21 @@ import React from 'react'
 import { reduxForm } from 'redux-form';
 import Field from '../../../admin/components/field.js'
 
-const my_details_form = ({...props, editMode, fields, handleSubmit, error}) => {
-  console.log('props', props);
-  return (
-    <form onSubmit={handleSubmit}>
-      {fieldList.map(field =>
-        <Field
-          {...fields[field]}
-          id={field}
-          mode='edit'
-          key={field}
-          readOnly={!editMode}
-          />
-      )}
-      {editMode ? edit_mode_buttons(props.toggle_edit_mode) : edit_button(props.toggle_edit_mode)}
-    </form>
-  )
-}
+const my_details_form = ({...props, editMode, fields, fieldList, handleSubmit, error}) => (
+  <form onSubmit={handleSubmit}>
+    {fieldList.map(field =>
+      <Field
+        {...fields[field]}
+        className={editMode ? 'user-input-editable' : 'user-input-read-only'}
+        id={field}
+        mode='edit'
+        key={field}
+        readOnly={!editMode}
+        />
+    )}
+    {editMode ? edit_mode_buttons(props.toggle_edit_mode) : edit_button(props.toggle_edit_mode)}
+  </form>
+)
 
 const edit_mode_buttons = (toggle) => (
   <div>
@@ -30,25 +28,6 @@ const edit_mode_buttons = (toggle) => (
 const edit_button = (toggle) => (
   <button onClick={toggle}>Edit</button>
 )
-
-const fieldList =
-[ 'title'
-, 'initials'
-, 'first_name'
-, 'last_name'
-, 'primary_email'
-, 'secondary_email'
-, 'address1'
-, 'address2'
-, 'address3'
-, 'address4'
-, 'county'
-, 'postcode'
-, 'deliverer'
-, 'home_phone'
-, 'work_phone'
-, 'mobile_phone'
-]
 
 export default reduxForm(
   { form: 'my_details_form'
