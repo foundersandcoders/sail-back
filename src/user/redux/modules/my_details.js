d1cdc8368ba31ea77cbb12f1a77585076bcb4157
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2016-08-02 11:11:29 +0100

    Successfully make put request and update member

--- src/user/redux/modules/my_details.js
+++ src/user/redux/modules/my_details.js
@@ -1,5 +1,5 @@
 import { createAction } from 'redux-actions'
-import { get_body } from 'app/http'
+import { get_body, put_body } from 'app/http'
 
 const CHANGE_TAB =
   'CHANGE_TAB'
@@ -20,7 +20,7 @@ const initialState =
 export default (state = initialState, { type, payload }) => {
   switch (type) {
     case CHANGE_TAB:
-      return { ...state, active_tab: payload } 
+      return { ...state, active_tab: payload }
     case FETCH_USER_DETAILS:
       return { ...state, user_details: payload }
     case SUBMIT_USER_DETAILS:
@@ -32,6 +32,55 @@ export default (state = initialState, { type, payload }) => {
   }
 }
 
+const newBody = {
+  "payments": [],
+  "events_booked": [],
+  "membership_type": {
+    "description": "Annual Single",
+    "value": "annual-single",
+    "amount": 1000,
+    "createdAt": "2016-08-02T09:45:51.000Z",
+    "updatedAt": "2016-08-02T09:45:51.000Z"
+  },
+  "id": 14030,
+  "title": "Mr",
+  "initials": "IG",
+  "last_name": "Gonzalez",
+  "first_name": "Juan",
+  "address1": "XYZ",
+  "address2": "XYZ",
+  "address3": "XYZ",
+  "address4": "XYZ",
+  "county": "XYZ",
+  "postcode": "E2 0SY",
+  "deliverer": null,
+  "home_phone": null,
+  "mobile_phone": null,
+  "work_phone": null,
+  "primary_email": "ivan@foundersandcoders.com",
+  "secondary_email": null,
+  "email_bounced": false,
+  "date_joined": "2016-08-01T23:00:00.000Z",
+  "date_membership_type_changed": null,
+  "life_payment_date": null,
+  "notes": null,
+  "gift_aid_signed": false,
+  "date_gift_aid_signed": null,
+  "gift_aid_cancelled": false,
+  "date_gift_aid_cancelled": null,
+  "standing_order": null,
+  "activation_status": "activated",
+  "news_type": "online",
+  "due_date": "2017-01-01T00:00:00.000Z",
+  "registered": "unregistered",
+  "deletion_date": null,
+  "privileges": "member",
+  "activation_date": null,
+  "createdAt": "2016-08-02T09:45:57.000Z",
+  "updatedAt": "2016-08-02T09:45:57.000Z",
+  "full_name": "Ivan Gonzalez"
+}
+
 export const change_tab =
   createAction(CHANGE_TAB)
 
@@ -39,7 +88,7 @@ export const fetch_user_details =
   createAction(FETCH_USER_DETAILS, () => get_body('/api/account'))
 
 export const submit_user_details =
-  createAction(SUBMIT_USER_DETAILS) //TODO make api call to update databas
+  createAction(SUBMIT_USER_DETAILS, () => put_body(newBody, '/api/account')) //TODO make api call to update databas
 
 export const toggle_edit_mode =
   createAction(TOGGLE_EDIT_MODE)
