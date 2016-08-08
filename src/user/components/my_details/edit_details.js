23b223283a691f7d8d19d0044ab334d566998aec
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2016-08-08 14:47:43 +0100

    Delete account_details tab and add user_field_structure to form_fields/member.js

--- src/user/components/my_details/edit_details.js
+++ src/user/components/my_details/edit_details.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import MemberFields from '../../../shared/dumb_components/member_fields.js'
 import { reduxForm } from 'redux-form'
-import { validate, fieldStructure, required, removed, read_only_user }
+import { validate, fieldStructure, required, removed, read_only_user, user_field_structure }
   from '../../../shared/form_fields/member.js'
 import buttons from '../my_details/buttons.js'
 
@@ -10,7 +10,7 @@ import { reject, contains, concat, __ } from 'ramda'
 export default ({ toggle_member_mode, mode, update_member_user, active_tab, ...props }) =>
   <ViewMember
     {...props}
-    fields={filter_fields(removed)(field_mapper[active_tab])}
+    fields={filter_fields(removed)(user_field_structure[active_tab])}
     Buttons={buttons}
     button_props={
       { edit_member_click: toggle_member_mode
@@ -33,8 +33,8 @@ const ViewMember = reduxForm(
 
 const filter_fields = to_be_removed => reject(contains(__, to_be_removed))
 
-const field_mapper =
-  { contact_details: concat(fieldStructure.address, fieldStructure.personal)
-  , membership_details: concat(['id'], fieldStructure.membership)
-  , account_details: concat(['id'], fieldStructure.edit)
-  }
+// const field_mapper =
+//   { contact_details: concat(fieldStructure.address, fieldStructure.personal)
+//   , membership_details: concat(['id'], fieldStructure.membership)
+//   , account_details: concat(['id'], fieldStructure.edit)
+//   }
--- src/user/redux/form_reducer.js
+++ src/user/redux/form_reducer.js
@@ -1,12 +1,12 @@
 /* @flow */
 const { reducer: form } = require('redux-form')
 import member_reducer from '../../shared/redux/modules/member.js'
-import { normalise as member } from '../../shared/form_fields/member.js'
+import { normalise as user } from '../../shared/form_fields/member.js'
 
 export default form.plugin(
   { user: member_reducer
   }
 ).normalize(
-  { member
+  { user
   }
 )
