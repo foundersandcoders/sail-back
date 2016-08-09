bd03abf01965906da1c9e6ec65e0af010dd731d4
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2016-08-09 12:14:23 +0100

    Make sure payments table fills out when loaded

--- src/user/containers/statements.js
+++ src/user/containers/statements.js
@@ -2,11 +2,22 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { pick } from 'ramda'
 
+import { fetch_member_user } from '../../shared/redux/modules/member.js'
+
 const PaymentsTable = require('../../shared/components/payments_table')
 
-const MemberPaymentsTable = ({payments}) =>
-  <div>
-    <PaymentsTable payments={payments} />
-  </div>
+class MemberPaymentsTable extends React.Component {
+  componentDidMount () {
+    this.props.fetch_member_user()
+  }
 
-export default connect(pick(['payments']))(MemberPaymentsTable)
+  render () {
+    return (
+      <div>
+        <PaymentsTable payments={this.props.payments} />
+      </div>
+    )
+  }
+}
+
+export default connect(pick(['payments']), { fetch_member_user })(MemberPaymentsTable)
