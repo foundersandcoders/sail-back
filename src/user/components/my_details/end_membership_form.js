fd1700ee2a21a34587a1306cbc1dc2fcf88af699
J Murphy <jmurphy.web@gmail.com>
2016-08-09 09:36:40 +0100

    set up deletion reason options.

--- src/user/components/my_details/end_membership_form.js
+++ src/user/components/my_details/end_membership_form.js
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import Field from '../../../shared/dumb_components/field.js'
+import { options } from '../../../shared/form_fields/member.js'
 
 export default ({fields, handleSubmit, reactivate_member, update_member_user, ...props}) => {
   return (
@@ -10,8 +11,15 @@ export default ({fields, handleSubmit, reactivate_member, update_member_user, ..
             </button>
           : <div>
               <p>
-                We are sorry to see you go. Please let us know why you are leaving.
+                Sorry to say goodbye.  It would help us if you could select the reason why you are leaving. And if you wish, add a comment
               </p>
+              <Field
+                {...fields.deletion_reason}
+                name={fields.deletion_reason.name}
+                options={options.deletion_reason}
+                description='Please givea a reason'
+                mode='edit'
+                />
               <textarea></textarea>
               <button type='submit' >
                 Confirm
