907d36ea89ba898e0deb8c2507a0c91d160405a3
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2016-08-11 11:58:14 +0100

    Complete validation and style form

--- src/user/components/change_password.js
+++ src/user/components/change_password.js
@@ -4,21 +4,25 @@ import { reduxForm } from 'redux-form'
 import ChangePasswordForm from '../components/change_password_form.js'
 
 export default ({update_member_user}) =>
-  <ChangePassword
+  <ChangePasswordReduxForm
     onSubmit={update_member_user}
   />
 
 // TODO: add validation and possibly change name of form from 'user'
 
 const validate = values => {
-  console.log('values ', values)
   const errors = {}
+  if (!values.password) {
+   errors.password = 'Required'
+ } else if (values.password.length < 6) {
+   errors.password = 'Must be at least 6 characters'
+ }
   if (values.password !== values.confirm_password) {
     errors.confirm_password = 'Passwords do not match'
   }
   return errors
 }
-const ChangePassword = reduxForm(
+const ChangePasswordReduxForm = reduxForm(
   { form: 'user'
   , validate
   , fields: ['password', 'confirm_password']
--- src/user/components/change_password_form.js
+++ src/user/components/change_password_form.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
-export default (props) => {
-  console.log('props', props.fields.confirm_password.error)
+export default ({ fields: { password, confirm_password }, ...props}) => {
   return (
     <form onSubmit={props.handleSubmit}>
-      <input type='password' {...props.fields.password}></input>
-      <input type='password'{...props.fields.confirm_password}></input>
-      <button type='submit'>Click</button>
+      <div className='change-password-inputs'>
+        <div className='input-title'><b>Enter new password</b></div>
+        <input type='password' className={password.touched && password.error ? 'red' : ''} {...password}></input>
+        {password.touched && password.error && <div className='error-message'>{password.error}</div>}
+        <div className='input-title'><b>Confirm new password</b></div>
+        <input type='password' className={confirm_password.touched && confirm_password.error ? 'red' : ''} {...confirm_password}></input>
+        {confirm_password.touched && confirm_password.error && <div className='error-message'>{confirm_password.error}</div>}
+      </div>
+      <button className='change-password-button' type='submit'>Submit</button>
     </form>
   )
 }
+
+// TODO: map inputs
+// TODO: show succesful response to changing password
