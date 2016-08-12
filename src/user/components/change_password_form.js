25c2e7bec5fe241ad711eb005a52c439d84dcd2b
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2016-08-12 16:15:03 +0100

    Map inputs in password change form.

--- src/user/components/change_password_form.js
+++ src/user/components/change_password_form.js
@@ -1,20 +1,23 @@
 import React from 'react'
 
-export default ({ fields: { password, confirm_password }, ...props}) => {
+const form_fields = [ 'password', 'confirm_password' ]
+
+export default ({fields, handleSubmit}) => {
   return (
-    <form onSubmit={props.handleSubmit}>
+    <form onSubmit={handleSubmit}>
       <div className='change-password-inputs'>
-        <div className='input-title'><b>Enter new password</b></div>
-        <input type='password' className={password.touched && password.error ? 'red' : ''} {...password}></input>
-        {password.touched && password.error && <div className='error-message'>{password.error}</div>}
-        <div className='input-title'><b>Confirm new password</b></div>
-        <input type='password' className={confirm_password.touched && confirm_password.error ? 'red' : ''} {...confirm_password}></input>
-        {confirm_password.touched && confirm_password.error && <div className='error-message'>{confirm_password.error}</div>}
+        {form_fields.map(input_maker(fields))}
       </div>
       <button className='change-password-button' type='submit'>Submit</button>
     </form>
   )
 }
 
-// TODO: map inputs
+const input_maker = fields => field =>
+  <div key={field}>
+    <div className='input-title'><b>Enter new password</b></div>
+    <input type='password' className={fields[field].touched && fields[field].error ? 'red' : ''} {...fields[field]}></input>
+    {fields[field].touched && fields[field].error && <div className='error-message'>{fields[field].error}</div>}
+  </div>
+
 // TODO: show succesful response to changing password
