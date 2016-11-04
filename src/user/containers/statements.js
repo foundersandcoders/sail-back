7840b5757e9e4f3a47e3e6070c64a04f38b83606
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2016-11-04 18:23:34 +0000

    Add add_donation redux action and link up to statements container.

--- src/user/containers/statements.js
+++ src/user/containers/statements.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { pick } from 'ramda'
 
 import { fetch_member_user } from '../../shared/redux/modules/member.js'
+import { add_donation } from '../redux/modules/user_payments.js'
 
 const PaymentsTable = require('../../shared/components/payments_table')
 
@@ -13,23 +14,27 @@ class MemberPaymentsTable extends React.Component {
 
   render () {
     return (
-      <div>
+      <div className='donation-section'>
         <PaymentsTable payments={this.props.payments} />
-        <DonationSection />
+        {this.props.user_payments.donation_made ? SuccessfulDonation(this.props.fetch_member_user) : DonationForm(this.props.add_donation)}
       </div>
     )
   }
 }
 
-const DonationSection = () => {
+const DonationForm = (add_donation) => {
   return (
-    <div className='donation-section'>
+    <div className='donation-form'>
       <h4>
         In order to make membership of the Friends available to as many people
         as possible we try to keep our annual subscription rates down. Perhaps
         you would like to help by making a donation.
       </h4>
-      <form>
+      <form onSubmit={(e) => {
+          e.preventDefault()
+          add_donation({ amount: e.target[0].value })
+      }}
+      >
         <input type='number' placeholder='£10' />
         <button type='submit'>Add donation to my account</button>
       </form>
@@ -37,4 +42,14 @@ const DonationSection = () => {
   )
 }
 
-export default connect(pick(['payments']), { fetch_member_user })(MemberPaymentsTable)
+// TODO: think of somehwere else to fire off this action
+
+const SuccessfulDonation = (fetch_member_user) => {
+  fetch_member_user()
+  return (
+    <div className='successful-donation'>
+      <h3>Your records have been updated</h3>
+    </div>
+  )
+}
+export default connect(pick(['payments', 'user_payments']), { fetch_member_user, add_donation })(MemberPaymentsTable)
