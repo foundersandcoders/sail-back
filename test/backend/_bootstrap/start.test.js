5df7190e57be9a71cd6d874b5c11f8235c66fbc7
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2016-11-14 12:49:38 +0000

    Delay test after sails have been lifted so that mocks can be created.

--- test/backend/_bootstrap/start.test.js
+++ test/backend/_bootstrap/start.test.js
@@ -3,9 +3,22 @@
 var test = require('tape')
 var server = require('./startServer.js')
 
+// test('Initialize server', function (t) {
+//   server(function (err, instance) {
+//     t.ok(instance, 'got instance')
+//     t.end()
+//   })
+// })
+
+// TODO: this setTimeout is there so that the hooks can run and the database
+// can be filled in. Should think of another way to do this really.
+
 test('Initialize server', function (t) {
+  t.plan(1)
   server(function (err, instance) {
-    t.ok(instance, 'got instance')
-    t.end()
+    setTimeout(function () {
+      t.ok(instance, 'got instance')
+      t.end()
+    }, 4000)
   })
 })
