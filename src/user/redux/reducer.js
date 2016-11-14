a60de82bea28e4039880a3cf4536bcd75b1dbbf7
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2016-11-14 10:49:13 +0000

    Get rid of unused tab component and redux module.

--- src/user/redux/reducer.js
+++ src/user/redux/reducer.js
@@ -1,11 +1,10 @@
 import { combineReducers } from 'redux'
 
-import my_details_form from './form_reducer.js'
+import personal_details_form from './form_reducer.js'
 
 //reducers
 import payments from '../../shared/redux/modules/payments.js'
 import mode from '../../shared/redux/modules/mode.js'
-import active_tab from './modules/active_tab.js'
 import member_reducer from '../../shared/redux/modules/member.js'
 import changed_password from '../../shared/redux/modules/change_password.js'
 import user_payments from './modules/user_payments.js'
@@ -13,10 +12,9 @@ import user_payments from './modules/user_payments.js'
 
 const root_reducer = combineReducers(
     { payments
-    , form: my_details_form
+    , form: personal_details_form
     , mode
-    , active_tab
-    , my_details: member_reducer
+    , personal_details: member_reducer
     , changed_password
     , user_payments
     }
