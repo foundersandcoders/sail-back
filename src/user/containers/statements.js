904e8ab0ac628bda9797ec285056fffcd43f4408
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2016-11-15 17:13:04 +0000

    Add change member functionality to statements section of member’s view.

--- src/user/containers/statements.js
+++ src/user/containers/statements.js
@@ -1,21 +1,31 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { pick, isEmpty, length } from 'ramda'
+import { pick, isEmpty, length, compose, join, map, split, toUpper, adjust, pipe } from 'ramda'
 
-import { fetch_member_user } from '../../shared/redux/modules/member.js'
+import { fetch_member_user, update_member_user } from '../../shared/redux/modules/member.js'
 import { add_donation } from '../redux/modules/user_payments.js'
 
 const PaymentsTable = require('../../shared/components/payments_table')
 
 class MemberPaymentsTable extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      membership_changed: false
+    }
+  }
   componentDidMount () {
     this.props.fetch_member_user()
   }
 
-  shouldComponentUpdate ({ user_payments: { donation_made }, payments }) {
-    return (
-      (this.props.user_payments.donation_made !== donation_made) || (length(this.props.payments) !== length(payments))
-    )
+  shouldComponentUpdate ({ user_payments: { donation_made }, payments, personal_details }) {
+    var donation_added = this.props.user_payments.donation_made !== donation_made
+    var payments_updated = length(this.props.payments) !== length(payments)
+    var membership_changed = this.props.personal_details.membership_type.value !== personal_details.membership_type.value
+
+    membership_changed && this.setState({ membership_changed: true })
+
+    return donation_added || payments_updated || membership_changed
   }
 
   componentDidUpdate () {
@@ -26,15 +36,56 @@ class MemberPaymentsTable extends React.Component {
     return (
       <div className='donation-section'>
         {isEmpty(this.props.payments) ? NoRecords() : <PaymentsTable payments={this.props.payments} />}
+        {this.state.membership_changed
+          ? SuccessfulMembershipChange(this.props.personal_details.membership_type.value)
+          : ChangeMembershipForm (this.props.update_member_user, this.props.personal_details.membership_type.value)
+        }
         {this.props.user_payments.donation_made ? SuccessfulDonation() : DonationForm(this.props.add_donation)}
       </div>
     )
   }
 }
 
+const ChangeMembershipForm = (update_member, membership_type) => {
+  return (
+    <div className='form-container'>
+      <h4>
+        Your membership is currently <b>{prettify_membership(membership_type)}</b>.
+        If you would like to change it please select one from the dropdown menu and
+        click 'Change Membership'.
+      </h4>
+      <form onSubmit={(e) => {
+          e.preventDefault()
+          return e.target[0].value !== '' && update_member({ membership_type: e.target[0].value })
+      }}
+      >
+        <select name='membership_type'>
+          <option value=''>Choose Membership</option>
+          <option value='annual-single'>Annual Single</option>
+          <option value='annual-double'>Annual Double</option>
+          <option value='annual-family'>Annual Family</option>
+          <option value='life-single'>Life Single</option>
+          <option value='life-double'>Life Double</option>
+        </select>
+        <button type='submit'>Change Membership</button>
+      </form>
+    </div>
+  )
+}
+
+const SuccessfulMembershipChange = (membership_type) => {
+  return (
+    <div className='successful-donation'>
+      <h3>
+        Your membership has been changed to <b>{prettify_membership(membership_type)}</b> and your payment records updated.
+      </h3>
+    </div>
+  )
+}
+
 const DonationForm = (add_donation) => {
   return (
-    <div className='donation-form'>
+    <div className='form-container'>
       <h4>
         In order to make membership of the Friends available to as many people
         as possible we try to keep our annual subscription rates down.
@@ -56,7 +107,7 @@ const DonationForm = (add_donation) => {
 const SuccessfulDonation = () => {
   return (
     <div className='successful-donation'>
-      <h3>Your records have been updated</h3>
+      <h3>Thanks for your donation. It has been added to your records.</h3>
     </div>
   )
 }
@@ -69,4 +120,6 @@ const NoRecords = () => {
   )
 }
 
-export default connect(pick(['payments', 'user_payments']), { fetch_member_user, add_donation })(MemberPaymentsTable)
+const prettify_membership = compose(join(' '), map(pipe(split(''), adjust(toUpper, 0), join(''))), split('-'))
+
+export default connect(pick(['payments', 'user_payments', 'personal_details' ]), { fetch_member_user, add_donation, update_member_user })(MemberPaymentsTable)
