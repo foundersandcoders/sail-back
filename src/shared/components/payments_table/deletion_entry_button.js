0b583787239d5adb71d3c3eb74162b09a988d6d5
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2016-11-30 13:08:41 +0000

    Small fixes so that payments and delete member functionality work on Internet Explorer.

--- src/shared/components/payments_table/deletion_entry_button.js
+++ src/shared/components/payments_table/deletion_entry_button.js
@@ -1,11 +1,11 @@
 var React = require('react')
 
 module.exports = ({ confirmation, which_text, which_delete }) =>
-  <div className={confirmation ? 'red' : 'grey'}>
-    <button
-      className={'small-button ' + (confirmation ? 'red' : '') }
-      onClick={which_delete()}
-    >
+  <button
+    className={'small-button ' + (confirmation ? 'red' : '') }
+    onClick={which_delete()}
+  >
+    <div className={confirmation ? 'red' : 'grey'}>
       {which_text()}
-    </button>
-  </div>
+    </div>
+  </button>
