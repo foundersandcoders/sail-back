f168a459c76f17384d6d0a9f359454d2bba67a97
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2016-12-05 11:07:11 +0000

    Add ‘do not reply’ message to custom email.

--- src/admin/dumb_components/custom_email_form.js
+++ src/admin/dumb_components/custom_email_form.js
@@ -2,10 +2,13 @@ import React from 'react'
 
 export default (email, submit) =>
   <div className='custom-email-container'>
-    <h3>Write out your custom email</h3>
+    <h3>Please include the message in the text box as the last line of your email</h3>
     <form onSubmit={submit}>
       <input type='text' className='custom-email-subject' placeholder='Subject' defaultValue={email ? email[0] : ''} />
-      <textarea className='custom-email' placeholder='Email body' defaultValue={email ? email[1] : ''} />
+      <textarea className='custom-email' placeholder='Email body' defaultValue={email ? email[1] : do_not_reply} />
       <button type='submit'>Preview Email</button>
     </form>
   </div>
+
+const do_not_reply =
+  'Please do not reply to messenger@friendsch.org which is not a monitored email address. If you have any queries please reply to membership@friendsch.org  .'
