7fd1f8d1cc3d10ee79f70bde5bfab7faf963ef91
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2016-12-06 17:29:01 +0000

    Rename ‘dumb_components’ in admin.

--- src/admin/components/custom_email_section.js
+++ src/admin/components/custom_email_section.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import preview_custom_email from '../dumb_components/preview_custom_email.js'
-import custom_email_form from '../dumb_components/custom_email_form.js'
+import preview_custom_email from '../components/preview_custom_email.js'
+import custom_email_form from '../components/custom_email_form.js'
 
 export default ({ custom_emails: { members, preview, mode }, preview_custom, ...props}) => {
   const onSubmit = e => {
