4f504270840526777368c54076819ec4f0fa3c21
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2017-01-10 16:55:10 +0000

    Reduxify open component so that redux-form can be used for sign up.

--- src/open/routes.js
+++ src/open/routes.js
@@ -1,24 +1,33 @@
 'use strict'
 
 var React = require('react')
-var ReactRouter = require('react-router')
+var { Router, Route, hashHistory } = require('react-router')
+var { createStore, applyMiddleware, compose } = require('redux')
+var { Provider } = require('react-redux')
+import future from 'redux-future'
 
 var App = require('./app.js')
 var Signin = require('./components/signin.js')
 
-var Router = ReactRouter.Router
-var Route = ReactRouter.Route
+// require().default is used here because the originating files use 'export default'
+// to expose their contents rather than 'module.exports'
 
-module.exports = function (h, onUpdate) {
+var Signup = require('./components/signup.js').default
+var reducer = require('./redux/reducer.js').default
 
-  onUpdate = onUpdate || function noop () {}
+var composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+var store = createStore(reducer, {}, composeEnhancers(applyMiddleware(future)))
 
+module.exports = () => {
   return (
-    <Router history={h} onUpdate={onUpdate}>
-      <Route component={App}>
-        <Route path='/' component={Signin} />
-        <Route path='/signin' component={Signin} />
-      </Route>
-    </Router>
+    <Provider store={store}>
+      <Router history={hashHistory}>
+        <Route component={App}>
+          <Route path='/' component={Signin} />
+          <Route path='/signin' component={Signin} />
+          <Route path='/signup' component={Signup} />
+        </Route>
+      </Router>
+    </Provider>
   )
 }
