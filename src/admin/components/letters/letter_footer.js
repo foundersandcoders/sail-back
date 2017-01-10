b6e9f3c293b336f0edc54109e2e964cb62dc00af
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2017-01-10 11:19:02 +0000

    Amend emails with new treasurer name and new methods of payment.

--- src/admin/components/letters/letter_footer.js
+++ src/admin/components/letters/letter_footer.js
@@ -4,7 +4,7 @@ export default () => (
   <div className='letter-footer'>
     <p>Sincerely,</p>
     <br />
-    Richard Evans, <br />
+    Chris Williams, <br />
     Treasurer, <br />
     Friends of Chichester Harbour <br />
   </div>
