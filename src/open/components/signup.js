83c97079c6f90b058ce4569e52a83f612c68b078
Ivan Gonzalez <ivangonzalez@rocketmail.com>
2017-01-13 15:17:44 +0000

    Refactor sign_up component.

--- src/open/components/signup.js
+++ src/open/components/signup.js
@@ -2,38 +2,24 @@
 const React = require('react')
 const { connect } = require('react-redux')
 const { reduxForm } = require('redux-form')
+const { identity } = require('ramda')
 
 const MemberFields = require('../../shared/components/member_fields.js')
 const { validate, sign_up_required, read_only_sign_up } = require('../../shared/form_fields/member.js')
 
-const { identity } = require('ramda')
-
 const { next_page, previous_page } = require('../redux/modules/page.js')
 const { sign_up } = require('../redux/modules/signup.js')
 
-const buttons = (props) =>
-  <div>
-    <button>Next</button>
-    <button onClick={props.previous_page} type='button'>Back</button>
-  </div>
-
-const AddMember = reduxForm(
-  { form: 'sign_up'
-  , validate: validate(sign_up_required)
-  , fields: []
-  }
-)(MemberFields)
-
-const NewMember = (props) => {
+const SignUp = (props) => {
   return (
     <div>
       <div>
-        <h1>Please fill in your details</h1>
-        <AddMember
+        <h2>{props.page === 5 ? 'If your details are correct click \'Submit\' otherwise go back to change them' : 'Please fill in your details'}</h2>
+        <SignUpForm
           fields={page_fields[props.page]}
           Buttons={buttons}
-          button_props={ { previous_page: props.previous_page } }
-          onSubmit={props.page.page === 5 ? props.sign_up : props.next_page}
+          button_props={ { previous_page: props.previous_page, page: props.page } }
+          onSubmit={props.page === 5 ? props.sign_up : props.next_page}
           required={sign_up_required}
           mode='edit'
           memberSignup
@@ -44,12 +30,29 @@ const NewMember = (props) => {
   )
 }
 
-const page_fields = [
-  [ 'title', 'first_name', 'last_name', 'initials' ],
-  [ 'address1', 'address2', 'address3', 'address4', 'postcode' ],
-  [ 'home_phone', 'mobile_phone' ],
-  [ 'primary_email', 'password' ],
-  [ 'membership_type' ],
-  [ 'title', 'first_name', 'last_name', 'initials', 'address1', 'address2', 'address3', 'address4', 'postcode', 'home_phone', 'mobile_phone', 'primary_email', 'membership_type' ]
-]
-export default connect(identity, { sign_up, next_page, previous_page })(NewMember)
+const SignUpForm = reduxForm(
+  { form: 'sign_up'
+  , validate: validate(sign_up_required)
+  , fields: []
+  }
+)(MemberFields)
+
+const buttons = (props) =>
+  <div>
+    {props.page !== 0 && back_button(props.previous_page)}
+    <button>{props.page === 5 ? 'Submit' : '>'}</button>
+  </div>
+
+const back_button = (previous_page) =>
+  <button onClick={previous_page} type='button'>{'<'}</button>
+
+const page_fields =
+  [ [ 'title', 'first_name', 'last_name', 'initials' ]
+  , [ 'address1', 'address2', 'address3', 'address4', 'postcode' ]
+  , [ 'home_phone', 'mobile_phone' ]
+  , [ 'primary_email', 'verify_email', 'password', 'verify_password' ]
+  , [ 'membership_type' ]
+  , read_only_sign_up
+  ]
+
+export default connect(identity, { sign_up, next_page, previous_page })(SignUp)
