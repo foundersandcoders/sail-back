2e01d8e20eec556cf23a5721acb98a6e94d121ac
Jack Murphy <jmurphy.web@gmail.com>
2017-03-27 09:29:31 +0300

    set up staging config

--- config/env/heroku-test.js
+++ config/env/heroku-test.js
@@ -3,11 +3,13 @@ databaseDetails.adapter = 'sails-mysql'
 
 module.exports = {
   connections: {
-    adapter: 'sails-mysql',
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    database: process.env.DB_DATABASE
+    testMySql: {
+      adapter: 'sails-mysql',
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      database: process.env.DB_DATABASE
+    }
   },
   models: {
     connection: 'testMySql',
