e9ede85aca3d1ed55ee049d4cdf054c2f8c491ff
Jack Murphy <jmurphy.web@gmail.com>
2017-03-27 13:42:22 +0300

    fix staging DATABASE for config

--- config/env/staging.js
+++ config/env/staging.js
@@ -5,12 +5,12 @@ module.exports = {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       host: process.env.DB_HOST,
-      database: process.env.DB_DATABASE
+      database: process.env.DATABASE
     }
   },
   models: {
     connection: 'testMySql',
-    migrate: 'safe'
+    migrate: 'drop'
   },
   session: {
     secret: '496a95f915e063812b4c39d698db2462',
