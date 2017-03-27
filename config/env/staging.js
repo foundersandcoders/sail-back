c02a8686c5cca99f1d5d8ff9353ba555ed626bcf
Jack Murphy <jmurphy.web@gmail.com>
2017-03-27 18:54:51 +0300

    ammend db_database env variable name to match production env

--- config/env/staging.js
+++ config/env/staging.js
@@ -5,7 +5,7 @@ module.exports = {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       host: process.env.DB_HOST,
-      database: process.env.DATABASE
+      database: process.env.DB_DATABASE
     }
   },
   models: {
