48f9bc897234f3257cd97a02293e931cabfeeeb4
Jack Murphy <jmurphy.web@gmail.com>
2017-03-28 09:21:58 +0300

    add login view whilst in maintanence mode

--- api/controllers/PublicController.js
+++ api/controllers/PublicController.js
@@ -8,6 +8,9 @@ var ForgotPass = require('../services/ForgotPass.js')
 var Mailgun = require('../services/email_mailgun')
 
 module.exports = {
+  adminLogin: function (req, res) {
+    res.view('pages/open', {user: req.session.user})
+  },
 
   showHome: function (req, res) {
     process.env.MAINTENANCE
