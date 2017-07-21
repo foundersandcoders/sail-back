11b3287d8ed71bd60fe65c64066746302c8647f6
Jack Murphy <jmurphy.web@gmail.com>
2017-07-21 17:56:19 +0100

    #516 - add measures to prevent dev accidentally connecting with live db in development mode

--- config/env/development.js
+++ config/env/development.js
@@ -1,4 +1,5 @@
 console.log('In Development!')
+var R = require('ramda')
 
 module.exports = {
   connections: {
@@ -7,12 +8,12 @@ module.exports = {
       host: 'localhost',
       user: 'root',
       password: 'correct',
-      database: process.env.DATABASE || 'foch_testing'
+      database: ensureNotHeroku(process.env.DATABASE)
     },
   },
   models: {
     connection: 'localMySql',
-    migrate: 'drop'
+    migrate: process.env.MIGRATE || 'safe'
   },
   session: {
     secret: '496a95f915e063812b4c39d698db2462',
@@ -22,4 +23,17 @@ module.exports = {
     rolling: true
   },
   port: process.env.PORT || 1337
+}
+
+function ensureNotHeroku (db_name) {
+  // default to foch_testing
+  if (!db_name) {
+    return 'foch_testing'
+  }
+  // do not allow dev environment to connect to live DB
+  else if (R.test(/heroku/, db_name)){
+    console.error('Database name cannot include "heroku" in development (ask Matt), using foch_testing')
+    return 'foch_testing'
+  }
+  return db_name
 }
