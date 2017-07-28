d79c73e3ee367a1df36116c0b5ed7aec142a55a3
Jack Murphy <jmurphy.web@gmail.com>
2017-07-28 14:59:53 +0100

    #522  - update container by connecting to reducer

--- src/admin/containers/subs_due.js
+++ src/admin/containers/subs_due.js
@@ -1,21 +1,17 @@
 /* @flow */
 const React = require('react')
 const { connect } = require('react-redux')
-const {  merge } = require('ramda')
+const {  merge, prop } = require('ramda')
 
 import sub_due_section from '../components/sub_due_section.js'
 
-import
-  { sub_due_tab
-  , SEND_SUB_REMINDER
-  } from '../redux/modules/email/reducer.js'
+import { update_subs_due } from '../redux/modules/subs_due.js'
 
-const Email = (props) => <div>Hello</div>
-//{sub_due_section(props)}
 
-const without_default = cb => e => {
-  e.preventDefault()
-  cb(e)
-}
+const SubsDue = (props) =>
+  <div> {sub_due_section(props)} </div>
 
-export default connect(null , { sub_due_tab })(Email)
+export default connect
+    (prop('subs_due')
+    , { update_subs_due })
+    (SubsDue)
