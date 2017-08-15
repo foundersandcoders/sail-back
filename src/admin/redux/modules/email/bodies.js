b6864fc91263eece679416b699337d753d070b24
Jack Murphy <jmurphy.web@gmail.com>
2017-08-15 09:51:37 +0200

    #512 - apply changes to balance overdue email.

--- src/admin/redux/modules/email/bodies.js
+++ src/admin/redux/modules/email/bodies.js
@@ -21,25 +21,14 @@ ${end_membership_email_footer}
 const late = n => ({ greeting, amount, id }) =>
 `Friends of Chichester Harbour
 Dear ${greeting}
-
-The renewal of your annual membership for the Friends of Chichester Harbour is now overdue ${n} days.  I very much hope that you will continue your membership in support of our voluntary activities around the harbour.  If you would like more details about the projects we support, or the work parties we organise, please visit our website at www.friendsch.org
-
-Your annual subscription is ${formatPounds(amount)} and payment can be made by one of the following three methods
-
-Credit Card or PayPal online from your PC, Mac, smartphone or tablet.
-To do so, go to http://friendsch.org and click on Member Sign-In on the Home page.
-
-Direct bank transfer to Friends of Chichester Harbour, Account No: 87037440,
-Sort Code 52-41-20 quoting your membership number ${id}
-
-Sending a cheque, quoting your membership number ${id} to Pam Marrs,
-Membership Secretary FOCH, 42 Bracklesham Road, Hayling Island PO11 9SJ
-
-If you do not wish to renew your membership or if you have any problems please let me know by emailing me at membership@friendsch.org.
+The renewal of your annual membership for the Friends of Chichester Harbour is now ${n} days overdue. We very much hope that you will continue your membership in support of our voluntary activities around the harbour.  Your membership subscription is critical to the significant financial assistance we give to the Conservancy and other organisations around the harbour.  The grants we provide support projects and activities that otherwise just would not take place, and help to conserve, preserve and educate for the future.  As well as the considerable financial assistance we give, the Friends provide work parties, every week all year round, which take part in essential maintenance and repairs to the footpaths and nature reserves across the harbour.  If you would like more details about the projects we support or the work parties we organise, please visit our website at www.friendsch.org
+Your annual subscription is ${formatPounds(amount)} and payment can be made by one of the following three methods:
+● Credit Card or PayPal online from your PC, Mac, smartphone or tablet.  To do so, go to http://friendsch.org and click on Member Sign-In on the Home page.
+● By direct bank transfer to Friends of Chichester Harbour, Account No: 87037440, Sort Code 52-41-20 quoting your membership number ${id}
+● By cheque, quoting your membership number ${id} to Pam Marrs, Membership Secretary FOCH, 42 Bracklesham Road, Hayling Island PO11 9SJ
+If you do not wish to renew your membership, or if you have any problems doing so, please let us know by emailing me at membership@friendsch.org
 Please do not reply to the messenger address above.
-
 If you have already paid, our apologies and please ignore this email.
-
 
 Yours sincerely,
 
